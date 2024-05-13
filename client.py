14c8191df4b1cb43758d484d4149e71eb8e42851
theB3NNDS <78061518+theB3NNDS@users.noreply.github.com>
2024-05-14 05:42:24 +0800

    feat: add word entry functionality

--- client.py
+++ client.py
@@ -11,9 +11,36 @@ pygame.display.set_caption("Word Explosion")
 timer = pygame.time.Clock()
 fps = 60
 
+#game variables
+active_string = 'test string'
+running = False
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+           'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+           'y', 'z']
+submit = ''
+
+#load in assets here
+title_font = pygame.font.Font('assets/BebasNeue-Regular.ttf', 50)
+typing_font = pygame.font.Font('assets/VT323-Regular.ttf', 50)
+system_font = pygame.font.Font('assets/Teko-VariableFont_wght.ttf', 30)
+
+class Button:
+    def __init__(self, x_pos, y_pos, text, clicked, surf):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.text = text
+        self.clicked = clicked
+        self.surf = surf
+
+    def draw(self):
+        cir = pygame.draw.circle(self.surf, (45, 89, 135), (self.x_pos, self.y_pos), 35)
+
+
 def draw_screen():
     #screen outlines
     pygame.draw.rect(screen, (40, 60, 82), [0, SCREEN_HEIGHT - 100, SCREEN_WIDTH, 100])
+    screen.blit(typing_font.render(f'"{active_string}"', True, 'white'), (270, SCREEN_HEIGHT -75))
+
 
 run = True
     
@@ -25,6 +52,18 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+        if event.type == pygame.KEYDOWN:
+            if not running:
+                if event.unicode.lower() in letters:
+                    active_string += event.unicode.lower()
+                if event.key == pygame.K_BACKSPACE and len(active_string) > 0:
+                    active_string = active_string[:-1]
+                if event.key == pygame.K_RETURN:
+                    submit = active_string
+                    active_string = ''
+            
+
     pygame.display.flip()
 pygame.quit()
 
